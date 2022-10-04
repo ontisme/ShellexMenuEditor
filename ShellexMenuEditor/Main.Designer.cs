2b082f3c6926f94cd3030eb56582f972c6ad99a8
ontisme <ontisme@gmail.com>
2022-10-04 18:42:09 +0800

    加入專案檔案。

---
+++ ShellexMenuEditor/Main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ShellexMenuEditor
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dgv_key_list = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.refreshToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_key_list)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgv_key_list
+            // 
+            this.dgv_key_list.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_key_list.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgv_key_list.ContextMenuStrip = this.contextMenuStrip1;
+            this.dgv_key_list.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgv_key_list.Location = new System.Drawing.Point(0, 0);
+            this.dgv_key_list.MultiSelect = false;
+            this.dgv_key_list.Name = "dgv_key_list";
+            this.dgv_key_list.ReadOnly = true;
+            this.dgv_key_list.RowTemplate.Height = 24;
+            this.dgv_key_list.Size = new System.Drawing.Size(286, 331);
+            this.dgv_key_list.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.refreshToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            // 
+            // refreshToolStripMenuItem
+            // 
+            this.refreshToolStripMenuItem.Name = "refreshToolStripMenuItem";
+            this.refreshToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.refreshToolStripMenuItem.Text = "Refresh";
+            this.refreshToolStripMenuItem.Click += new System.EventHandler(this.refreshToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 331);
+            this.Controls.Add(this.dgv_key_list);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ShellexMenuEditor";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_key_list)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_key_list;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem refreshToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ ShellexMenuEditor/Main.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShellexMenuEditor
+{
+    public partial class Main : Form
+    {
+        public string key_base = @"*\shellex\ContextMenuHandlers";
+        public Main()
+        {
+            InitializeComponent();
+            InitKeyList();
+        }
+
+        public void InitKeyList()
+        {
+            dgv_key_list.Rows.Clear();
+            foreach (var i in GetSubKeys())
+            {
+                dgv_key_list.Rows.Add(i);
+            }
+        }
+
+        public string[] GetSubKeys()
+        {
+            RegistryKey registryKey = Registry.ClassesRoot.CreateSubKey(key_base);
+            var sub_key_list = registryKey.GetSubKeyNames();
+            return sub_key_list;
+        }
+
+        public void DelSubKey(string key_name)
+        {
+            RegistryKey key = Registry.ClassesRoot;
+
+            key.DeleteSubKey($"{key_base}\\{key_name}", true);
+            key.Close();
+        }
+
+        private void refreshToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InitKeyList();
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var index = dgv_key_list.CurrentCell.RowIndex;
+            var key_name = dgv_key_list.Rows[index].Cells[0].Value.ToString();
+            DelSubKey(key_name);
+            MessageBox.Show("Deleted, may require a reboot to take effect");
+            InitKeyList();
+        }
+    }
+}
